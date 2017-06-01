d41a6cb0983164f02cb7862b75e2672dca8dbdaa
Randy Gelhausen <rgelhau@gmail.com>
2017-06-01 08:38:05 -0400

    Simplified classes and config, using NiFi 1.3 snapshot, added README

---
+++ README.md
@@ -0,0 +1,9 @@
+Build:
+```
+mvn clean package
+```
+
+Run:
+```
+java -jar target/NiFi-S2S-Example-0.0.1-SNAPSHOT.jar
+```
---
+++ src/main/java/com/github/randerzander/S2SExample.java
@@ -0,0 +1,74 @@
+package com.github.randerzander;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.nifi.remote.Transaction;
+import org.apache.nifi.remote.TransactionCompletion;
+import org.apache.nifi.remote.TransferDirection;
+import org.apache.nifi.remote.client.SiteToSiteClient;
+import org.apache.nifi.remote.protocol.SiteToSiteTransportProtocol;
+
+import org.apache.nifi.remote.protocol.DataPacket;
+
+public class S2SExample {
+  public static void main(String[] args){
+    SiteToSiteClient.Builder builder = new SiteToSiteClient.Builder();
+		builder.url("http://localhost:1026/nifi");
+		builder.portName("source1");
+    builder.transportProtocol(SiteToSiteTransportProtocol.HTTP);
+		builder.nodePenalizationPeriod(60, TimeUnit.SECONDS);
+		SiteToSiteClient client = builder.build();
+
+    String content = "Hello, world";
+    HashMap<String,String> attributes = new HashMap<String,String>();
+    attributes.put("whoami", "java client");
+    try{
+      Transaction transaction = client.createTransaction(TransferDirection.SEND);
+      transaction.send(new SimplePacket(content, attributes));
+      transaction.confirm();
+      TransactionCompletion completion =  transaction.complete();
+      System.out.println("Bytes transferred: " + completion.getBytesTransferred());
+    }catch(Exception e){
+      e.printStackTrace(System.out);
+    }
+  }
+
+  private static class SimplePacket implements DataPacket{
+    private final InputStream input;
+    private final long size;
+    private final Map<String,String> attributes;
+    
+    public SimplePacket(final String data, final Map<String,String> attributes) throws IOException{
+      if(data!=null){
+        this.size = data.length();
+        this.input= new ByteArrayInputStream(data.getBytes());
+      }else{
+        this.size=0;
+        this.input = new ByteArrayInputStream(new byte[0]);
+      }
+      
+      if(attributes==null)
+        this.attributes = new HashMap<String,String>();
+      else
+        this.attributes=attributes;
+    }
+
+    public Map<String, String> getAttributes() {
+      return this.attributes;
+    }
+
+    public InputStream getData() {
+      return this.input;
+    }
+
+    public long getSize() {
+      return this.size;
+    }
+  }
+}
